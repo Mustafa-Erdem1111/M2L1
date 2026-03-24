3d54b5e5dc46d9dfee548950a4d58a0cb33baf98
Mustafa-Erdem1111 <merdemboluk@gmail.com>
2026-03-24 12:42:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,39 +14,6 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
 
-
-@bot.command()
-async def mem(ctx):
-    with open('images/mem1.jpg', 'rb') as f:
-        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
-        picture = discord.File(f)
-   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
-    await ctx.send(file=picture)
-
-@bot.command()
-async def mem2(ctx):
-    with open('images/mem2.jpg', 'rb') as f:
-        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
-        picture = discord.File(f)
-   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
-    await ctx.send(file=picture)
-
-
-@bot.command()
-async def mem3(ctx):
-    with open('images/mem3.png', 'rb') as f:
-        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
-        picture = discord.File(f)
-   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
-    await ctx.send(file=picture)
-
-@bot.command()
-async def random_mem(ctx):
-    my_mem = random.choice(os.listdir('images'))
-    with open(f'images/{my_mem}', 'rb') as f:
-            picture = discord.File(f)
-    await ctx.send(file=picture)
-
 def get_duck_image_url():    
     url = 'https://random-d.uk/api/random'
     res = requests.get(url)
@@ -60,7 +27,7 @@ async def duck(ctx):
     image_url = get_duck_image_url()
     await ctx.send(image_url)   
     
-@bot.command()
+@bot.command('animal')
 async def animal(ctx):
     with open('images/animal1.jpg', 'rb') as f:
         # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
@@ -79,12 +46,18 @@ async def animal2(ctx):
 
 @bot.command()
 async def animal3(ctx):
-    with open('images/animal3.png', 'rb') as f:
+    with open('images/animal3.jpg', 'rb') as f:
         # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
         picture = discord.File(f)
    # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
     await ctx.send(file=picture)
 
+@bot.command()
+async def random_animal(ctx):
+    my_animal = random.choice(os.listdir('images'))
+    with open(f'images/{my_animal}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
 
 
 bot.run(TOKEN)
